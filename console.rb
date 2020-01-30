af9a64d4c8c73d542c2561543656a589d13751dc
howmehow <howmehow@gmail.com>
2020-01-30 14:12:57 +0000

    Initial commit

--- console.rb
+++ console.rb
@@ -4,7 +4,13 @@ require_relative("models/star")
 
 require("pry")
 
+star1 = Star.new({'first_name' => 'Michael',
+  'last_name' => 'Douglas' })
 
+movie1 = Movie.new({'title' => 'Falling down', 'genre' => 'Drama/Crime'})
+
+casting1 = Casting.new({'star_id' => star1.id,
+  'movie_id' => movie1.id, 'fee' => 2000})
 
 
 
